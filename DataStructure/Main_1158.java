c7a5850e1cdbda8ad0245b405c408166c8708b33
118dg <54856465+118dg@users.noreply.github.com>
2021-04-29 15:20:15 +0900

    Update Main_1158.java

--- DataStructure/Main_1158.java
+++ DataStructure/Main_1158.java
@@ -1,65 +1,47 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+ 
 public class Main_1158 {
-
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-//      StringBuilder answer = new StringBuilder();
-
-		Scanner sc = new Scanner(System.in);
-
-//		int total = Integer.parseInt(br.readLine());
-//		int n = Integer.parseInt(br.readLine());
-		int total = sc.nextInt();
-		int n = sc.nextInt();
-
-		int index = n;
-		int count = 0;
-		int print = 0;
-
-		int[] numArr = new int[total + 10];
-		for (int i = 1; i <= total; i++) {
-			numArr[i - 1] = i;
-		}
-
-		// answer.append("<" + numArr[n - 1] + ", "); //첫 번째 숫자
-		System.out.print("<" + numArr[n - 1] + ", ");
-		print++;
-		numArr[n - 1] = 0;
-
-		while (print != total && -1 < index && index < total) { // 모든 숫자 출력될 때까지
-
-			if (numArr[index] != 0) {
-				count++;
-			}
-
-			if (count == n) { // n번째 꺼마다 숫자 출력
-				count = 0;
-				if (print == total - 1) {
-					// answer.append(numArr[index] + ">"); //마지막 숫자
-					System.out.print(numArr[index] + ">");
-					print++;
-					numArr[index] = 0; // 쓴 숫자는 없애기
-				} else {
-					// answer.append(numArr[index] + ", ");
-					System.out.print(numArr[index] + ", ");
-					print++;
-					numArr[index] = 0; // 쓴 숫자는 없애기
-				}
-			}
-
-			index++;
-			if (index % total == 0)
-				index = 0; // 인덱스 배열 끝 도달시 처음으로
-		}
-
-		// bw.write(answer.toString() + "\n");
-		// bw.flush();
-		// bw.close();
-		// br.close();
-	}
-
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+ 
+        Queue<Integer> q = new LinkedList<>();
+        
+        // Queue에 1~N까지 값을 offer한다.
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+ 
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        
+        // Queue의 사이즈가 1일 때까지 반복한다.
+        while(q.size() != 1) {
+            // K - 1번째까지는 처음에 있던 값을 맨 뒤로 보낸다.
+            for (int i = 0; i < K - 1; i++) {
+                q.offer(q.poll());
+            }
+            // K번째 값은 poll한 후 출력한다.
+            sb.append(q.poll() + ", ");
+        }
+        
+        // Queue의 사이즈가 1일 때는 단순히 poll하면 된다.
+        sb.append(q.poll() + ">");
+ 
+        bw.write(sb.toString() + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
